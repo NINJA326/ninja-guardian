2c4263c991792f1435a02d35788ef0c14e2b6e58
NINJA326 <xxxmastsxxx@gmail.com>
2026-09-08 13:44:36 +0900

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-window.NINJA_GUARDIAN_CONFIG = Object.freeze({
-  APP_NAME: 'NINJA AIRS 保護者',
-  APP_VERSION: 'step25-shell-v1',
-  API_URL:
-    'https://script.google.com/macros/s/AKfycbxbnca_UIigFLiSfTGOtlU4871H_cm9rHy8iutFT5P9HJR5w3TBvBoA0IZ5fQWkUJRw/exec'
-});
