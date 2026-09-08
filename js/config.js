6b5d851b9722bffb5f44f8f39ca3d6870ea49618
NINJA326 <xxxmastsxxx@gmail.com>
2026-09-08 13:58:57 +0900

    Update APP_VERSION and add LIFF_ID to config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,11 @@
 
 window.NINJA_GUARDIAN_CONFIG = Object.freeze({
   APP_NAME: 'NINJA AIRS 保護者',
-  APP_VERSION: 'step25-shell-v1',
+  APP_VERSION: 'step28-liff-config-v1',
+
+  LIFF_ID:
+    '2010789200-KayhN3KT',
+
   API_URL:
     'https://script.google.com/macros/s/AKfycbxbnca_UIigFLiSfTGOtlU4871H_cm9rHy8iutFT5P9HJR5w3TBvBoA0IZ5fQWkUJRw/exec'
 });
